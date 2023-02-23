5a3f207f7bb2e8416b3190fd0cd8ed18717a1b84
Joegitrussell <112443220+Joegitrussell@users.noreply.github.com>
2023-02-23 00:23:40 +0000

    updated

--- components/Experience.jsx
+++ components/Experience.jsx
@@ -82,7 +82,7 @@ const Experience = () => {
     ]
 
   return (
-    <div id="Experience" className="w-full">
+    <div id="experience" className="w-full">
       <div className='max-w-screen-xl mx-auto px-8 py-16
         text-center md:text-left'>
             <h2 className='text-5xl md:text-7xl tracking-wider
