5de688742ad9a478989a813f980af1aec38f3544
Joegitrussell <112443220+Joegitrussell@users.noreply.github.com>
2023-03-12 23:57:14 +0000

    error resolved

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import HomePage from '../components/HomePage.jsx'
 import Portfolio from '@/components/Portfolio.jsx'
 import Experience from '@/components/Experience.jsx'
 import Me from '@/components/Me.jsx'
-import Contact from '@/components/contact.jsx'
+import Contact from '@/components/Contact.jsx'
 
 export default function Home() {
   return (
