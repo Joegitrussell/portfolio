9656ae6a4785f90d2279326de0fcb0356f8e5563
Joegitrussell <112443220+Joegitrussell@users.noreply.github.com>
2023-03-13 00:32:55 +0000

    explanation section

--- components/Portfolio.jsx
+++ components/Portfolio.jsx
@@ -14,12 +14,14 @@ const Portfolio = () => {
             title: ' The Foodbank Network',
             imageSrc: foodbank,
             url: "https://foodbanknetwork.vercel.app",
+            explanation:"A Four week group project at the end of the Bootcamp. A Foodbank Finder App allowing users to type in a location and return a list of Foodbanks in the area utilising a Foodbank API and the Google Maps API as well as an Supabase SQL backend for comments and community functionality. UI/UX based upon market research to keep the App easy to use with low cognitive load and fully mobile responsive. Tech stack: Next.js, Tailwind, Supabase SQL, Supabase Auth, Jest, React testing library, Cypress and Vercel"
         },
         {
           id: 2,
           title: ' The Study Hub',
           imageSrc: studyhub,
           url: "https://joerussell-week9-teamproject.vercel.app/",
+          explanation:"A One week group project at Week 8 of the Bootcamp. Enables students to easily receive more information around study topics and then comment to enable communication. Made exclusively for desktop use. Tech stack: React, CSS, Node.JS, Express, Jest, Cypress and Vercel"
       },
       {
         id: 1,
@@ -42,8 +44,9 @@ const Portfolio = () => {
             <div className='max-w-5xl mx-auto grid md:grid-cols-2
             gap-8'>
 
-            {portfolios.map(({id, title, imageSrc, url}) => (
+            {portfolios.map(({id, title, imageSrc, url, explanation}) => (
              //   <Link key ={id} href={`/portfolio/${url}`}>
+             <div>
              <Link key ={id} href={url} target="_blank">
                     <div className="cursor-pointer group shadow-ms
                     shadow-grey-600 overflow-hidden round-md">
@@ -53,6 +56,8 @@ const Portfolio = () => {
                         </h2>
                     </div>
                 </Link>
+                <p>{explanation}</p>
+                </div>
             ))}
 
             </div>
