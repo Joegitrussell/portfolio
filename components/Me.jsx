794ad56986909854c1a6a2d6f06333f87ca73a1c
Joegitrussell <112443220+Joegitrussell@users.noreply.github.com>
2023-03-13 09:00:43 +0000

    text update

--- components/Me.jsx
+++ components/Me.jsx
@@ -6,7 +6,7 @@ const Me = () => {
     <div id="me" className="w-full">
       <div className='max-w-screen-xl mx-auto  px-8 py-20
         text-center md:text-left'>
-            <h2 className='text-5xl md:text-2xl tracking-wider
+            <h2 className='text-7xl md:text-2xl tracking-wider
             uppercase text-blue-500 font-bold'>
                About Me
             </h2>
@@ -14,30 +14,30 @@ const Me = () => {
                 <h1 className='py-4 max-w-3xl mx-auto'>Whats my story?</h1>
                 <p className='py-4 max-w-2xl mx-auto'>
                     I have been working as a Traditional Engineer for the last 16 years, I have been very fortunate having completed
-                    an Advanced Apprenticeship within a Oil Refinery and then worked there untill it shut down. I then taught myself
-                     the skills needed to work as a Gas engineer and became self employed. During this time to fill my desire to 
-                     learn and fulfil a ambition I earned a degree through the Open University in Engineering specialising in Energy
+                    an Advanced Apprenticeship within a Oil Refinery and then worked there until it shut down. I then taught myself
+                     the skills needed to work as a Gas engineer and became self-employed. <br></br>During this time to fill my desire to 
+                     learn and fulfil a lifelong ambition I earned a degree through the Open University in Engineering specialising in Energy
                       and sustainability. I'm proud to say I did this whilst working full time running my own business and raising two amazing kids.
                     
                 </p>
                 <h1 className='py-4 max-w-3xl mx-auto'>Why Tech?</h1>
                 <p className='py-4 max-w-2xl mx-auto'>
                    After 10 years of running my own business I found myself longing for a challenge, it was a good job but it wasn't making
-                   me happy and my brain wasnt engaged. I knew I had to make a change so I took the plunge and made arrangements to close my business.
+                   me happy and my brain wasn't engaged. I knew I had to make a change so I took the plunge and made arrangements to close my business.<br></br>
                    For some time I had been doing courses on CodeAcademy and playing on Codepen after suggestions from friends and I made
-                   a amazing discovery. Code was fantastic, I am engaged and challenged and I wanted to know more, it also was a brilliant way to use the engineering and problem solving skills I have been honing over the last 16 years. After researching the best way to
-                    learn code for a career I came across bootcamps, and I applied to a couple. I was extremely fortunate to be offered a place
-                    at the renowned School of Code, a 16 week fully funded intensive bootcamp run by Dr. Chris Meah, I later found out more than 4000 people applied and I was
+                   an amazing discovery. <br></br>Code was fantastic! I am engaged and challenged and I wanted to know more, it also was a brilliant way to use the engineering and problem solving skills I have been honing over the last 16 years. <br></br>After researching the best way to
+                    learn code for a career I came across Bootcamps.<br></br> I was extremely fortunate to be offered a place
+                    at the renowned School of Code, a 16 week full stack, fully funded intensive Bootcamp run by Dr. Chris Meah, I later found out more than 4000 people applied and I was
                      one of the few to be enrolled and it was the best thing that could have happened!
                 </p>
                 <h1 className='py-4 max-w-3xl mx-auto'>What am i looking for?</h1>
                 <p className='py-4 max-w-2xl mx-auto'>
-                    I am looking for my first job in tech. The School of code trained and taught us to be full stack developers so I am comfortable with both
+                    I am looking for my first role in tech. The School of code trained and taught us to be full stack developers so I am comfortable with both
                      the front end and the back end. During the course I was constantly working with people every day, in pairs and groups so I am
-                     very used to working with others. I am very interested in differnt stacks and languages, I have even started to learn Java and MongoDB in my 
-                     spare time! I particulary enjoyed backend and testing at the bootcamp and found myself to be a excellent problem solver and bug fixer. I am just looking
+                     very used to working with others. <br></br>I am very interested in different stacks and languages, I have even started to learn Java and MongoDB in my 
+                     spare time and research Data Engineering! I particularly enjoyed backend and testing at the Bootcamp and found myself to be a excellent problem solver and bug fixer.<br></br> I am looking
                      for someone to give me a chance, I am new to coding but not to work and problem solving, I have a proven work history of working really well with others
-                     and being dedicated to my career, I am especially proud of the fact that I have only worked for two companies in the last 16 years!
+                     and being dedicated to my career, I am especially proud of the fact that I have only worked for two companies in the last 16 years! I will strive to become as asset to whomever I work for.
 
                 </p>
                 <h1 className='py-4 max-w-2xl mx-auto'>Please reach out and get in contact, my CV can be viewed below!</h1>
